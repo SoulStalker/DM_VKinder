46c295121b3346d671992f2957f94c5e0e8e1050
Almaz Galimov <almazwork@gmail.com>
2023-04-22 18:43:48 +0500

    little updates

--- vk_api_utils.py
+++ vk_api_utils.py
@@ -54,20 +54,27 @@ class VkFront:
                         keyboard = self.create_keyboard()
                         self.write_msg(user_id,
                                        f'Найдено {len(res_users)} пользователей.Нажмите "Дальше" для просмотра следующего пользователя', kb=keyboard)
+                        if len(res_users) == 0:
+                            self.write_msg(user_id,
+                                           f'Нажмите "Поиск" для повторного поиска',
+                                           kb=keyboard)
                         users = self.api.search_users(self.params)
                         for user in users:
                             db_handler = Handling(user_id, user['id'], user['photo_url'])
                             if not db_handler.is_person_in_db():
                                 res_users.append(user)
-                    elif request == 'дальше' and res_users:
-                        user = res_users.pop(0)
-                        db_handler = Handling(user_id, user['id'], user['photo_url'])
-                        photos_user = self.api.get_top_photos(user['id'])
-                        keyboard = self.create_keyboard(one_time=True)
-                        self.write_msg(user_id, f'{user["name"]}nСсылка на профиль: https://vk.com/id{user["id"]}',
-                                       photos_user, kb=keyboard)
-                        db_handler.save_search_results()
-                        # offset += 1
+                    elif request == 'дальше':
+                        if not res_users:
+                            self.write_msg(user_id, f'Нажмите "Поиск" для начала поиска', kb=keyboard)
+                        else:
+                            user = res_users.pop(0)
+                            db_handler = Handling(user_id, user['id'], user['photo_url'])
+                            photos_user = self.api.get_top_photos(user['id'])
+                            keyboard = self.create_keyboard(one_time=True)
+                            self.write_msg(user_id, f'{user["name"]}\nСсылка на профиль: https://vk.com/id{user["id"]}',
+                                           photos_user, kb=keyboard)
+                            db_handler.save_search_results()
+                            # offset += 1
                     else:
                         keyboard = self.create_keyboard()
                         self.write_msg(user_id, "Неизвестный запрос", kb=keyboard)

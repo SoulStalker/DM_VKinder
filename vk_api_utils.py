751c9c5f1b2804742282cb78bee0ac7af18e3d15
Almaz Galimov <almazpython@gmail.com>
2023-04-24 14:23:36 +0500

    end request for missung information

--- vk_api_utils.py
+++ vk_api_utils.py
@@ -13,6 +13,7 @@ class VkFront:
         self.interface = vk_api.VkApi(token=gr_token)
         self.api = VkBackend(vk_token)
         self.params = None
+        self.res_users = []
 
     def create_keyboard(self, one_time=False):
         keyboard = VkKeyboard(one_time=one_time)
@@ -45,7 +46,6 @@ class VkFront:
     def bot(self):
         while True:
             offset = 0
-            res_users = []
             request, user_id = self.vk_long_poll()
             self.params = self.api.get_user_info(user_id)
             if request == "привет" or request == "старт":
@@ -58,52 +58,57 @@ class VkFront:
                 keyboard = self.create_keyboard()
                 required_params = ['sex', 'bdate', 'home_town']
                 params = self.params
+
+                print(params)
+
                 for param in required_params:
                     if not params[param]:
-                        print(param)
                         if param == 'sex':
+                            self.write_msg(user_id, 'Введите ваш пол "мужской" или "женский":')
+                            request, user_id = self.vk_long_poll()
                             if request == 'мужской':
                                 self.params[param] = 1
-                                break
+                                # break
                             elif request == 'женский':
                                 self.params = 2
-                                break
-                            else:
-                                self.write_msg(user_id, 'Введите ваш пол "мужской" или "женский":')
+                                # break
                         elif param == 'bdate':
+
+                            print(self.params[param])
+
                             self.write_msg(user_id, 'Введите дату вашего рождения:')
-                            self.params[param] = '08.08.1988'
-                            break
+                            request, user_id = self.vk_long_poll()
+                            self.params[param] = request
+                            # break
                         elif param == 'home_town':
                             self.write_msg(user_id, 'Введите город поиска:')
-
                             request, user_id = self.vk_long_poll()
-
                             self.params[param] = request
-                            break
+                            # break
                 users = self.api.search_users(self.params)
-                print(self.params)
+                offset += 1000
+
                 for user in users:
                     db_handler = Handling(user_id, user['id'], user['photo_url'])
                     if not db_handler.is_person_in_db():
-                        res_users.append(user)
+                        self.res_users.append(user)
 
                 self.write_msg(user_id,
-                               f'Найдено {len(res_users)} пользователей.'
+                               f'Найдено {len(self.res_users)} пользователей.'
                                f'Нажмите "Дальше" для просмотра следующего пользователя', kb=keyboard)
+
             elif request == 'дальше':
                 keyboard = self.create_keyboard()
-                if not res_users:
+                if not self.res_users:
                     self.write_msg(user_id, f'Нажмите "Поиск" для начала поиска', kb=keyboard)
                 else:
-                    user = res_users.pop(0)
+                    user = self.res_users.pop()
                     db_handler = Handling(user_id, user['id'], user['photo_url'])
                     photos_user = self.api.get_top_photos(user['id'])
                     keyboard = self.create_keyboard(one_time=True)
                     self.write_msg(user_id, f'{user["name"]}\nСсылка на профиль: https://vk.com/id{user["id"]}',
                                    photos_user, kb=keyboard)
                     db_handler.save_search_results()
-                    offset += 1
             else:
                 keyboard = self.create_keyboard()
                 self.write_msg(user_id, "Неизвестный запрос", kb=keyboard)
